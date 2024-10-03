1ca2ce4f24dc55de08163d227a8a80f4ff3c4d59
Tristan Tritch <ttmeep101@hotmail.com>
2024-10-03 18:44:57 -0500

    completes tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main2 = document.querySelector("main");
+main2.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="Tristan is the champion";
+document.body.append(newHeader);
